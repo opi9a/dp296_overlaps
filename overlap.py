8621e63937220f0f0a3a7b917d6119b4413ea255
opi9a <opi9a@gavinroberts.com>
2017-02-18 16:19:36 +0000

    Update overlap.py

--- overlap.py
+++ overlap.py
@@ -1,21 +1,35 @@
-import os
-os.system('clear')
-
+# First get input, as two strings
 print("\n\nCALCULATE OVERLAPPING AREA BETWEEN TWO RECTANGLES")
 print("\nEnter two co-ordinates for each rectangle, with x and y separated by a comma.")
 print("For example:  1,1 5,5\n")
 A_in = input("Enter first rectangle co-ordinates: ") 
 B_in = input("Enter second rectangle co-ordinates: ")
 
+# Now we need to do some processing to convert the strings into lists
+# of floats that can be passed to the primary function
+
+# So begin with an empty list for each rectangle
 A = []
 B = []
+
+# Split the string into the two points (default is to split at spaces),
+# then iterate through the two points.  NB after the split, there will be two subtrings.
+# For each point, split the substring (this time at the comma), 
+# and use a list comprehension to return a list of floats.
+# Add each list of floats (there will be two, one for each point) to the empty list.
+# Result is a list with two elements (one for each point), 
+# each of which is a list with two floats (one for each co-ordinate).
 
 for point in A_in.split():
   A.append([float(i) for i in point.split(",")])
 
+# And we need to do it for each rectangle
+
 for point in B_in.split():
   B.append([float(i) for i in point.split(",")])
 
+# Now we can call the real function, passing it the two rectangles
+  
 def overlaps(A, B):
 
 # Basic idea and insight is that the overlapping area will be the product of
@@ -51,4 +65,4 @@ def overlaps(A, B):
 try:
     print("\nYour overlap is ", overlaps(A,B),"\n")
 except:
-    print("\nI can't tell you an area because you can't have typed the inputs properly.  I think it's time you started taking this a bit more seriously\n\n\n")
+    print("\nI can't tell you an area because you can't have typed the inputs properly. I think it's time you started taking this a bit more seriously\n\n\n")
